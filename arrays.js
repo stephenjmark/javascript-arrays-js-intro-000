14886ad67c042ffd5584864ca6474aca7178c15e
Stephen Mark <stephenjmark@gmail.com>
2017-11-07 10:28:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array){
+  newArray = array.slice(1)
+  return newArray
+}
